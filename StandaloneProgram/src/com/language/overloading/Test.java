beafa4fee2dc7f94216329f319b81fd6cc1e8224
alekharouta <techie.alekh@yahoo.com>
2018-10-25 17:31:04 +0530

    Test Commited

---
+++ StandaloneProgram/src/com/language/overloading/Test.java
@@ -0,0 +1,11 @@
+package com.language.overloading;
+
+public class Test {
+
+	public static void main(String[] args) {
+//It is a Constructor Overload Test Class
+		ConstructorOverload cl=new ConstructorOverload("A");
+		
+	}
+
+}
