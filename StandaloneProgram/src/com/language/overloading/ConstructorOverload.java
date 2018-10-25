93e10b9fb09c4f6a22fa83197e2b17f014875640
alekharouta <techie.alekh@yahoo.com>
2018-10-25 17:27:29 +0530

    Commited

---
+++ StandaloneProgram/src/com/language/overloading/ConstructorOverload.java
@@ -0,0 +1,18 @@
+package com.language.overloading;
+
+public class ConstructorOverload {
+
+	public ConstructorOverload() {
+System.out.println("...");
+	}
+
+	public ConstructorOverload(String name) {
+		System.out.println("Name");
+	}
+
+	public ConstructorOverload(int price) {
+		System.out.println("Price");
+		//Hello
+	}
+
+}
